6d646df16675178f773d21253800eef16993c18e
rosaura <rosaurayanapari123@gmail.com>
2024-06-06 23:28:57 -0500

    Reapply "com"
    This reverts commit 2828e3dc0fb03aeac59d4b0d5c447f152f8407c7.

--- ms-monitoreo_asistencia/src/main/java/com/example/msmonitoreo_asistencia/repository/EstudianteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.msmonitoreo_asistencia.repository;
-
-import com.example.msmonitoreo_asistencia.entity.Estudiante;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EstudianteRepository extends JpaRepository<Estudiante,Integer> {
-}
