bd748572415c244330543efb9599d46db5e9b933
david robert <davidrobertyucramamani@gmail.com>
2024-06-08 18:30:20 -0500

    config ya terminado la recilencia

--- ms-monitoreo_asistencia/src/main/java/com/example/msmonitoreo_asistencia/feign/EstudianteFeign.java
+++ ms-monitoreo_asistencia/src/main/java/com/example/msmonitoreo_asistencia/feign/EstudianteFeign.java
@@ -1,7 +1,9 @@
 package com.example.msmonitoreo_asistencia.feign;
 
 
+import com.example.msmonitoreo_asistencia.dto.DocenteDto;
 import com.example.msmonitoreo_asistencia.dto.EstudianteDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "ms-gestionestudiantes-service", path = "/estudiante")
 public interface EstudianteFeign {
     @GetMapping("/{id}")
-    ResponseEntity<EstudianteDto> buscarPorId(@PathVariable(required = true) Integer id) ;
+    @CircuitBreaker(name = "clienteListarPorIdCB", fallbackMethod = "fallbackEstudiante")
+    public ResponseEntity<EstudianteDto> buscarPorId(@PathVariable(required = true) Integer id);
 
+    default ResponseEntity<EstudianteDto> fallbackEstudiante(Integer id, Exception e) {
+
+
+        return ResponseEntity.ok(new EstudianteDto());
+    }
 }
