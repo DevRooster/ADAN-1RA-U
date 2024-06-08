1c39b7e92a3093f3f87425a67b38863ef98792aa
rosaura <rosaurayanapari123@gmail.com>
2024-06-07 20:20:24 -0500

    actualizacion de matriculas y docente

---
+++ ms-admatricula/src/main/java/com/example/msadmatricula/dto/EstudianteDto.java
@@ -0,0 +1,19 @@
+package com.example.msadmatricula.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Data
+public class EstudianteDto {
+    private Integer id;
+    private String nombre;
+    private LocalDate fechaNacimiento;
+    private String direccion;
+    private String telefono;
+    private String email;
+    private String gradoActual;
+
+
+}
---
+++ ms-admatricula/src/main/java/com/example/msadmatricula/feign/EstudiantesFeign.java
@@ -0,0 +1,19 @@
+package com.example.msadmatricula.feign;
+
+import com.example.msreportanalisis.dto.EstudianteDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "ms-gestiondocentes-service", path = "/estudiante")
+public interface EstudiantesFeign {
+    @GetMapping("/{id}")
+    @CircuitBreaker(name = "clienteListarPorIdCB", fallbackMethod = "fallbackEstudiante")
+    public ResponseEntity<EstudianteDto> buscarPorId(@PathVariable(required = true) Integer id) ;
+    default ResponseEntity<EstudianteDto> fallbackEstudiante (Integer id, Exception e) {
+
+        return ResponseEntity.ok(new EstudianteDto());
+    }
+}
