b669c55d83fa62008741adebc8c9d8a6cfad05b8
david robert <davidrobertyucramamani@gmail.com>
2024-06-10 01:24:15 -0500

    el pdf de matri

--- ms-matricula/src/main/java/com/example/msmatricula/utils/PdfHorario.java
+++ ms-matricula/src/main/java/com/example/msmatricula/utils/PdfHorario.java
@@ -24,6 +24,25 @@ public class PdfHorario {
             Font boldFont = new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD);
             Paragraph paragraph = new Paragraph(horario.getNombre(), boldFont);
             document.add(paragraph);
+            Paragraph paragraphDescripcion = new Paragraph("Descripcion: " + horario.getDescripcion());
+            document.add(paragraphDescripcion);
+
+            // Add GradoDesignado
+            Paragraph paragraphGradoDesignado = new Paragraph("Grado Designado: " + horario.getGradoDesigando());
+            document.add(paragraphGradoDesignado);
+
+            // Add EstudianteId
+            Paragraph paragraphEstudianteId = new Paragraph("Estudiante ID: " + horario.getEstudianteId());
+            document.add(paragraphEstudianteId);
+
+            // Add EstudianteDto information if available
+            if (horario.getEstudianteDto() != null) {
+                Paragraph paragraphEstudianteNombre = new Paragraph("Estudiante Nombre: " + horario.getEstudianteDto().getNombre());
+                document.add(paragraphEstudianteNombre);
+
+                Paragraph paragraphEstudianteApellido = new Paragraph("Estudiante Apellido: " + horario.getEstudianteDto().getDireccion());
+                document.add(paragraphEstudianteApellido);
+            }
         }
         document.add(new Paragraph("\n"));
         // Write data rows
