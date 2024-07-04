85583b4c690b91b697e5dd6394bb64c73da6d35c
EnriqueMV18 <enriquemv937@gmail.com>
2024-07-04 18:56:28 -0500

    Borre Matriculas

--- ms-matricula/src/main/java/com/example/msmatricula/repository/HorarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.msmatricula.repository;
-
-
-import com.example.msmatricula.entity.Horario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HorarioRepository extends JpaRepository<Horario, Integer> {
-    
-}
--- ms-matricula/src/main/java/com/example/msmatricula/repository/MatriculaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.msmatricula.repository;
-
-
-import com.example.msmatricula.entity.Matricula;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MatriculaRepository extends JpaRepository<Matricula, Integer> {
-    
-}
