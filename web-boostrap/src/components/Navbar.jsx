a46303dff4a51c7f77b498f950843ecb2466b803
EnriqueMV18 <enriquemv937@gmail.com>
2024-07-05 03:13:03 -0500

    Disabled Navbar Matriculas Horario Reportes v2

--- web-boostrap/src/components/Navbar.jsx
+++ web-boostrap/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/dashboard">Micro System</Link>
                 <button
